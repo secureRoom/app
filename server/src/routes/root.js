dd0a2f73f72402136efaca484b21bb3533943def
Serhii <35103391+SKaistrenko@users.noreply.github.com>
2019-03-26 20:13:17 -0400

    serving html on / route

--- server/src/routes/root.js
+++ server/src/routes/root.js
@@ -1,4 +1,6 @@
+const path = require('path');
+
 module.exports = (_, res, fs) => {
   res.setHeader('Content-Type', 'text/html');
-  return res.end(fs.readFileSync(__dirname.concat('client/')));
+  return res.end(fs.readFileSync(path.join(__dirname, './../../../client/index.html')));
 };
--- server/src/test/integration.test.js
+++ server/src/test/integration.test.js
@@ -2,13 +2,15 @@ const request = require('supertest');
 
 const server = 'http://localhost:3000';
 
-test('should pass integration tests', (done) => {
-  request(server)
-    .get('/')
-    .expect(200)
-    .expect(' Content-Type', /text\/html/)
-    .end((err) => {
-      if (err) throw done(err);
-      done();
-    });
+describe('should pass integration tests', () => {
+  test('should serve html file on \'/\' endpoint', (done) => {
+    request(server)
+      .get('/')
+      .expect(200)
+      .expect('Content-Type', /text\/html/)
+      .end((err) => {
+        if (err) throw done(err);
+        done();
+      });
+  });
 });
